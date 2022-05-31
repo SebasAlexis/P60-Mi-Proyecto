50900acfef0d4fd072ed2da42a5f615917339eb1
Alexis Lomas <alomasm@est.ups.edu.ec>
2022-05-31 10:34:27 -0500

    Creada la clase Persona

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+#include "persona.h"
+
 using namespace std;
 
 int main()
 {
+    Persona p;
+    p.nombre="Alexis";
+    p.edad=20;
     cout << "Mi primer proyecto en Git y Github!" << endl;
+    cout<<p.nombre<<endl;
+    cout<<p.edad<<endl;
     return 0;
 }
---
+++ persona.h
@@ -0,0 +1,16 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+
+#include <string>
+
+using namespace std;
+
+class Persona
+{
+public:
+    string nombre;
+    int edad;
+    Persona();
+};
+
+#endif // PERSONA_H
